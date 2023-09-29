3ca5d784e8534cb5e0a109b69b5c547c85651969
alexis656 <144554820+alexis656@users.noreply.github.com>
2023-09-29 19:01:51 -0300

    Create tarea_casa.py

---
+++ tarea_casa.py
@@ -0,0 +1,15 @@
+def double_letter(str):
+    # Resultado
+    result = ''
+    for letter in str:
+        result += letter * 2
+    return result
+
+def secret_function(a, b):
+    # Escriba aquí su código
+    result = a + b
+    return result
+
+print(secret_function("¡que bendición, ", "lo e logrado!"))
+print(secret_function(1, 2))
+print(secret_function("¡Hola, ", "Mundo!"))
